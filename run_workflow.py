6af5e99ed2d585820a08aef95b39dde8871d475f
airahiredly <aira@hiredly.com>
2025-07-15 16:29:51 +0800

    Update run_workflow.py

--- run_workflow.py
+++ run_workflow.py
@@ -38,25 +38,67 @@ except ValueError:
 
 # Connect to Snowflake
 conn = snowflake.connector.connect(
-    user=SNOWFLAKE_USER,
-    password=SNOWFLAKE_PASSWORD,
-    account=SNOWFLAKE_ACCOUNT,
-    warehouse=SNOWFLAKE_WAREHOUSE,
-    database=SNOWFLAKE_DATABASE,
-    schema=SNOWFLAKE_SCHEMA
+    user="AIRA",
+    password="UKs6f9TAUfDR@f3",
+    account="A4216615408961-LK73781",
+    warehouse="COMPUTE_WH",
+    database="INTERMEDIATE",
+    schema="N8N"
 )
 cursor = conn.cursor()
 
+# Get current date (or datetime if needed)
 recommend_at = datetime.now().strftime('%Y-%m-%d')
-
-executed_candidate = cursor.execute("""SELECT JOB_ID from intermediate.n8n.internal_job_candidate_recs""")
-executed_candidate_list = pandas.DataFrame(executed_candidate)
-newlist = executed_candidate_list[0]
-string_list = newlist.tolist()
-quoted_list = [f'"{item}"' for item in string_list]
 
 # Process each job
 for job_global_id in [row[global_id_index] for row in rows if len(row) > global_id_index]:
+    executed_candidate = cursor.execute("""with jobs as (
+select
+title
+, id as job_id
+, global_id as job_global_id
+from base.postgresql_hiredly_my.jobs 
+where company_id = '44ea6c51-c84d-423f-8649-96cb592ea995'
+and is_active = true
+and lower(title) not like '%career fair%'
+)
+
+, applied_candidates as (
+select
+j.job_global_id
+, array_agg(u.global_id) as user_global_id
+from base.postgresql_hiredly_my.job_applications ja
+inner join jobs j on j.job_id = ja.job_id
+inner join base.postgresql_hiredly_my.users u on u.id = ja.user_id
+group by j.job_global_id
+)
+
+, recommended_candidates as (
+select 
+job_id as job_global_id
+, array_agg(candidate_id) as user_global_id 
+from intermediate.n8n.internal_job_candidate_recs
+group by job_global_id
+)
+
+select
+    array_distinct(
+    array_cat(
+      coalesce(ac.user_global_id, array_construct()),
+      coalesce(rc.user_global_id, array_construct())
+    )
+  ) as user_global_id
+from recommended_candidates rc
+left join applied_candidates ac on ac.job_global_id = rc.job_global_id
+where rc.job_global_id = '661e6499c33b0f5ac80caff0'
+                                        
+                                        
+                                        """)
+    executed_candidate_list = pandas.DataFrame(executed_candidate)
+    newlist = executed_candidate_list[0]
+    string_list = newlist.tolist()
+    # Print as a quoted list (e.g. ["id1", "id2", ...])
+    quoted_list = [f'"{item}"' for item in string_list]
     try:
         api_response = requests.post(POST_ENDPOINT, json={
             "global_id": job_global_id,
