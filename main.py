d8faa5362e92bfb30621b5a820d6cfd610ee9224
airahiredly <aira@hiredly.com>
2025-08-25 17:13:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -154,7 +154,7 @@ for job_global_id in job_lookup.keys():
                 time.sleep(5)
 
                 cursor.execute("""
-                    INSERT INTO intermediate.n8n.internal_job_candidate_recs_staging (job_id, recommend_at, candidate_id)
+                    INSERT INTO intermediate.n8n.internal_job_candidate_recs (job_id, recommend_at, candidate_id)
                     SELECT %s, %s, parse_json(%s)
                 """, (job_global_id, recommend_at, f'"{candidate}"'))
 
